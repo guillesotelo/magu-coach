1cf94d8480ea3b93ecc5514da0983b6c06f45545
Guillermo Sotelo <guillermo.sotelo@volvocars.com>
2024-05-24 09:26:59 +0200

    wpp-mobile

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,6 @@
-import React, { useContext, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import BG from '../../assets/images/site/BG.jpg'
-import Button from '../../components/Button/Button'
 import { useHistory } from 'react-router-dom'
-import { APP_COLORS } from '../../constants'
-import { AppContext } from '../../AppContext'
 import Whatsapp from '../../assets/icons/whatsapp.svg'
 import WhatsappChat from '../../components/WhatsappChat/WhatsappChat'
 import ServiceCard from '../../components/ServiceCard/ServiceCard'
@@ -14,7 +11,6 @@ export default function Home({ }: Props) {
   const [showWhatsapp, setShowWhatsapp] = useState(false)
   const [showChat, setShowChat] = useState(false)
   const history = useHistory()
-  const { isMobile } = useContext(AppContext)
   const message = 'Hola Agustina! Me gustaría ponerme en contacto contigo para conocer más sobre tus servicios.'
 
   useEffect(() => {
@@ -37,8 +33,7 @@ export default function Home({ }: Props) {
           style={{
             backgroundImage: `url(${BG})`,
             backgroundSize: 'cover',
-            filter: showChat ? 'contrast(.3)' : 'contrast(1)',
-            overflow: showChat ? 'hidden' : '',
+            filter: showChat ? 'contrast(.3)' : 'contrast(1)'
           }}>
 
           <div className="home__section-wrap">
